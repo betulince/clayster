ca09cf1dca37cc6b10eeb82c938fd2c81bd41d6a
bince <bince@deloitte.nl>
2023-08-02 15:54:22 +0200

    First commit

---
+++ src/main/java/com/example/clayster/ClaysterApplication.java
@@ -0,0 +1,13 @@
+package com.example.clayster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaysterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClaysterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clayster/ClaysterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clayster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaysterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
